00ddc3b02bf3b5a6f2d381abd772ec5b24e3f005
Wolpert Tobias GCR EOQMM4 <tobias.wolpert@gmail.com>
2022-08-25 11:38:53 -0400

    Started header component

---
+++ src/components/Layout/Button.js
@@ -0,0 +1,17 @@
+import React from "react";
+import CartIcon from "../Cart/CartIcon";
+import classes from "./Button.module.css";
+
+const Button = (props) => {
+  return (
+    <button className={classes.button}>
+      <span className={classes.icon}>
+        <CartIcon />
+      </span>
+      <span>Your Cart</span>
+      <span className={classes.badge}>3</span>
+    </button>
+  );
+};
+
+export default Button;
---
+++ src/components/Meals/MealItem/MealItemForm.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const MealItemForm = (props) => {
+  return <form></form>;
+};
+
+export default MealItemForm;
